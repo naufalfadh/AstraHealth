5eeef9f74909c4540470699a4a870ba295242f49
Al Ghifari <alghifarimuhammad165@gmail.com>
2024-01-19 22:28:43 +0700

    tes #6

--- AstraHealth/Models/PasienModel.cs
+++ AstraHealth/Models/PasienModel.cs
@@ -20,8 +20,7 @@ namespace AstraHealth.Models
         public string? anm_nama_admin { get; set; }
         public List<PemakaianObatModel> PemakaianObats { get; set; } = new List<PemakaianObatModel>();
     }
-
-    public class PemakaianObatModel
+public class PemakaianObatModel
     {
         public string pmo_id { get; set; }
         public string pmo_id_anamnesa { get; set; }
@@ -29,5 +28,29 @@ namespace AstraHealth.Models
         public int pmo_jumlah { get; set; }
         public string pmo_satuan { get; set; }
         public DateTime? pmo_tanggal { get; set; }
+    }
+
+    public class DiagnosaSakit
+    {
+        public string diagnosa { get; set; }
+        public int jumlah { get; set; }
+    }
+
+    public class PemakaianObat
+    {
+        public string nama_obat { get; set; }
+        public int jumlah { get; set; }
+    }
+
+    public class ProdiDanDepartemen
+    {
+        public string prodi_dan_departemen { get; set; }
+        public int jumlah { get; set; }
+    }
+
+    public class KecelakaanKerjaDanRujukan
+    {
+        public int kecelakaan_kerja { get; set; }
+        public int rujukan { get; set; }
     }
 }
